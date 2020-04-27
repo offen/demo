a6e0e1c807b22f72ef51912a7054c0ac6752bf5c
Frederik Ring <frederik.ring@gmail.com>
2020-04-27 19:42:20 +0200

    add script

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # demo
 Script to download and start an Offen demo
+
+---
+
+This script is availabe at `https://demo.offen.dev` and can be used as a convenience shortcut for running an Offen demo on your system:
+
+```sh
+curl -sS https://demo.offen.dev | bash
+```
+
+---
+
+### Upload the file to AWS S3
+
+Using valid credentials you can update the script using:
+
+```
+$ aws s3 cp demo.sh s3://offen-demo
+```
---
+++ demo.sh
@@ -0,0 +1,45 @@
+#! /bin/bash
+
+set -eo pipefail
+
+download () {
+  echo "Downloading binaries into /tmp/offen-demo-$1 ..."
+  mkdir -p /tmp/offen-demo-$1 && cd /tmp/offen-demo-$1
+  curl -sSL https://get.offen.dev/latest | tar -xz
+  echo ""
+  echo "Verifying download checksums ..."
+  md5sum -c checksums.txt
+  echo ""
+  echo "Done preparing. Next we'll be launching your demo."
+}
+
+pull () {
+  echo ""
+  echo "Using Docker ..."
+  docker pull offen/offen:latest
+}
+
+run_demo () {
+  case "$1" in
+      Linux*)
+        download $(date +"%s")
+        ./offen-linux-amd64 demo
+      ;;
+      Darwin*)
+        download $(date +"%s")
+        ./offen-darwin-10.6-amd64 demo
+      ;;
+      *)
+        if [ -x "$(command -v docker)" ]; then
+          pull
+          docker run --rm -it -p 9876:9876 offen/offen:latest demo -port 9876
+        else
+          echo "Your operating is currently not supported by this script."
+          echo "You can try installing Docker and use the offen/offen image to run a demo."
+          echo ""
+          echo "$ docker run --rm -it -p 9876:9876 offen/offen:latest demo -port 9876"
+        fi
+  esac
+}
+
+run_demo $(uname -s)
