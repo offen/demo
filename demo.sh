5e6fdeac35d698cb4559386fd2eeb5bbfe77ce51
Frederik Ring <frederik.ring@gmail.com>
2020-04-27 21:10:08 +0200

    fix indentation

--- demo.sh
+++ demo.sh
@@ -21,24 +21,24 @@ pull () {
 
 run_demo () {
   case "$1" in
-      Linux*)
-        download $(date +"%s")
-        ./offen-linux-amd64 demo
-      ;;
-      Darwin*)
-        download $(date +"%s")
-        ./offen-darwin-10.6-amd64 demo
-      ;;
-      *)
-        if [ -x "$(command -v docker)" ]; then
-          pull
-          docker run --rm -it -p 9876:9876 offen/offen:latest demo -port 9876
-        else
-          echo "Your operating is currently not supported by this script."
-          echo "You can try installing Docker and use the offen/offen image to run a demo."
-          echo ""
-          echo "$ docker run --rm -it -p 9876:9876 offen/offen:latest demo -port 9876"
-        fi
+    Linux*)
+      download $(date +"%s")
+      ./offen-linux-amd64 demo
+    ;;
+    Darwin*)
+      download $(date +"%s")
+      ./offen-darwin-10.6-amd64 demo
+    ;;
+    *)
+      if [ -x "$(command -v docker)" ]; then
+        pull
+        docker run --rm -it -p 9876:9876 offen/offen:latest demo -port 9876
+      else
+        echo "Your operating is currently not supported by this script."
+        echo "You can try installing Docker and use the offen/offen image to run a demo."
+        echo ""
+        echo "$ docker run --rm -it -p 9876:9876 offen/offen:latest demo -port 9876"
+      fi
   esac
 }
 
