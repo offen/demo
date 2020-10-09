f02d4883c6605c9865075e1e3d4e7ce5934cc03b
Frederik Ring <frederik.ring@gmail.com>
2020-10-09 14:28:33 +0200

    simplify checking for active docker daemon

--- demo.sh
+++ demo.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 # Copyright 2020 - Offen Authors <hioffen@posteo.de>
 # SPDX-License-Identifier: MPL-2.0
 
@@ -8,7 +9,7 @@ download () {
   echo "Downloading binaries into /tmp/offen-demo-$1 ..."
   mkdir -p /tmp/offen-demo-"$1" && cd /tmp/offen-demo-"$1"
   curl -sSL https://get.offen.dev | tar -xz
-  if [ -x "$(command -v md5sum)" ]; then
+  if [[ $(command -v md5sum) ]]; then
     echo ""
     echo "Verifying download checksums ..."
     md5sum -c checksums.txt
@@ -34,22 +35,16 @@ run_demo () {
       ./offen-darwin-10.6-amd64 demo
     ;;
     *)
-      if [ -x "$(command -v docker)" ]; then
-        set +e
-        docker info > /dev/null 2>&1; ec=$?
-        set -e
-        case $ec in
-          0)
-            pull
-            docker run --rm -i -p 9876:9876 offen/offen:latest demo -port 9876
-          ;;
-          *)
-            echo "We tried to use Docker for your demo, but it seems it is currently not running."
-            echo "Please start Docker on your system and try running this script again."
-            ;;
-        esac
+      if [[ $(command -v docker) ]]; then
+        if [[ ! $(docker info) ]]; then
+          echo "We tried to use Docker for your demo, but it seems it is currently not running."
+          echo "Please start Docker on your system and try running this script again."
+          exit 1
+        fi
+        pull
+        docker run --rm -i -p 9876:9876 offen/offen:latest demo -port 9876
       else
-        echo "Your operating is currently not supported by this script."
+        echo "Your operating system is currently not supported by this script."
         echo "You can try installing Docker and use the offen/offen image to run a demo."
         echo ""
         echo "$ docker run --rm -it -p 9876:9876 offen/offen:latest demo -port 9876"
